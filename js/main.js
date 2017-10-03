082d0b3118789644078950b135c489212e3d28d3
Anthematics <jasonharder2@gmail.com>
2017-10-03 16:44:17 -0400

    added small code comments

--- js/main.js
+++ js/main.js
@@ -5,11 +5,14 @@ function haveclicked() {
  let switch2 = document.querySelector('.switch');
  let status = document.querySelector('.status')
 
+//if toggled off -> do this
+
  if (switch2.classList.toggle('off')) {
 	status.innerHTML = "lights out"
 	body.className = 'dark'
 	}
 
+// if toggled on -> do this.
  else if (switch2.classList.contains('on')) {
 	status.innerHTML = "It's bright"
 	body.className = 'light'
