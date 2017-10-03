45fccf55ffa7f7ad259a1e2b61c8c17ce53b4d23
Anthematics <jasonharder2@gmail.com>
2017-10-03 15:53:38 -0400

    text now changes when switch does

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-
 document.querySelector('.switch').addEventListener('click',haveclicked);
 
 function haveclicked() {
+ let dark = document.querySelector('.dark')
  let light = document.querySelector('.switch');
+ let status = document.querySelector('.status')
+
  light.classList.toggle('on');
  light.classList.toggle('off');
+
+ if (light.classList.contains('off'))
+	status.innerHTML = "lights out"
+
+
+ else if (light.classList.contains('on'))
+	status.innerHTML = "its bright"
+
 }
