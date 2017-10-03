d4e5ee835cc486756c570dba3a1fe7a1926c617b
Anthematics <jasonharder2@gmail.com>
2017-10-03 16:44:39 -0400

    fixed spacing

--- js/main.js
+++ js/main.js
@@ -6,12 +6,10 @@ function haveclicked() {
  let status = document.querySelector('.status')
 
 //if toggled off -> do this
-
  if (switch2.classList.toggle('off')) {
 	status.innerHTML = "lights out"
 	body.className = 'dark'
 	}
-
 // if toggled on -> do this.
  else if (switch2.classList.contains('on')) {
 	status.innerHTML = "It's bright"
